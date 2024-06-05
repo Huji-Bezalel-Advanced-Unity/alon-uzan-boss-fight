c6e6ac2d577bc84a486aef1775568a7a6704942c
אלון אוזן <uzano101@MacBook-Pro-sl-lwn.local>
2024-06-05 13:50:42 +0300

    add animators

---
+++ Assets/_Alon/Scripts/Core/Managers/BaseAnimator.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using Spine.Unity;
+
+
+namespace _Alon.Scripts.Core.Managers
+{
+    public class BaseAnimator
+    {
+        protected BaseAnimator(){}
+
+        public virtual void SetAnimation(GameObject player, String animationName, bool loop)
+        {
+            var skeletonAnimation = player.GetComponent<SkeletonAnimation>();
+            if (skeletonAnimation == null)
+            {
+                Debug.LogError("Failed to get SkeletonAnimation component.");
+                return;
+            }
+
+            skeletonAnimation.AnimationState.SetAnimation(0, animationName, loop);
+        }
+    }
+}
---
+++ Assets/_Alon/Scripts/Core/Managers/PlayerAnimator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace _Alon.Scripts.Core.Managers
+{
+    public class PlayerAnimator : BaseAnimator
+    {
+        private readonly HashSet<String> _animations = new HashSet<String>
+        {
+            "Death",
+            "Hurt",
+            "Slash",
+            "Stab",
+            "Idle",
+            "Run",
+            "Walk"
+        };
+        
+        public PlayerAnimator(){}
+
+        public override void SetAnimation(GameObject player, String animationName, bool loop)
+        {
+            if (!_animations.Contains(animationName))
+            {
+                Debug.LogException(new Exception("Invalid animation name."));
+            }
+            base.SetAnimation(player, animationName, loop);
+        }
+    }
+}
